d77ca445bb506977e96920c7a0849599e94158b3
mauriciosalazarsh <85299691+mauriciosalazarsh@users.noreply.github.com>
2024-05-19 19:50:53 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,11 @@
 from pydantic import BaseModel
-class Item(BaseModel):
+from datetime import date
+
+class Student(BaseModel):
+    nombre: str
+    apellido: str
+    fecha_de_nacimiento: date
+
+class Item(BaseModel):  # Si necesitas esta clase para otros propósitos, la dejo aquí.
     name: str
     age: int
